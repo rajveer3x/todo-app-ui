2ba48d4a5eb7e8b971094bc0e9e4ed78013e0cd8
rajveer3x <panwarrajveer07@gmail.com>
2026-02-08 10:17:56 +0530

    added css module

--- src/components/AppName.jsx
+++ src/components/AppName.jsx
@@ -1,6 +1,8 @@
+import styles from "./AppName.module.css"
+
 function AppName(){
-  return <h1>TODO App</h1>
+  return <h1 className={styles.todoHeading}>TODO App</h1>
 
 }
 
-export default AppName;
+export default AppName; 
